3a8d5d1d1f7e8cbc2ce5a8b35aed848b76eef544
SillyBits <sillybits@onlinehome.de>
2019-06-01 12:02:42 +0200

    Minor refactoring and cleanup

--- Util/Callback.py
+++ Util/Callback.py
@@ -1,71 +1,52 @@
 
-'''
-Provides sort of callback object to deal with messaging over threads and
-allows for sending 'Start', 'Update' and 'End' notifications.
-(mainly to inform UI to update something, e.g. progress bar when loading a save)
-'''
+from wx \
+	import PostEvent, ID_ANY
 
-import wx
-import wx.lib.newevent
+from wx.lib \
+    import newevent
 
-
-'''
-def NewEvent():
-    """
-    Generates a new `(event, binder)` tuple.
-
-    ::
-
-        MooEvent, EVT_MOO = NewEvent()
-
-    """
-
-    evttype = wx.NewEventType()
-
-    class _Event(wx.PyEvent):
-        def __init__(self, **kw):
-            wx.PyEvent.__init__(self)
-            self.SetEventType(evttype)
-            self._getAttrDict().update(kw)
-
-    return _Event, wx.PyEventBinder(evttype)
-'''
 
 
 class Callback:
+	"""
+    Provides some callback object to deal with messaging over threads and
+    allows for sending 'Start', 'Update' and 'End' notifications.
+    (mainly to inform UI to update, e.g. a progress bar when loading a save)
+    """
 
 	def __init__(self, target=None, func=None, interval=1000):
+		"""
+	    :param target: Window to send events to (using .Bind)
+	    :param func: Function to be triggered
+	    :param interval: Interval for letting update messages pass
+	       (This is to reduce event load if .update is being called way to often)
+	    """
 		if not Callback.__EvtCls:
-			Callback.__EvtCls,Callback.__Evt = wx.lib.newevent.NewEvent()
+			Callback.__EvtCls, Callback.__Evt = newevent.NewEvent()
 		if target and func:
 			self.bind(target, func)
 		self.__interval = interval
 
-	#def __del__(self):
-	#	if self.__target:
-	#		self.unbind()
-	#-> causes an exception: TypeError: 'NoneType' object is not iterable
-
 
 	def bind(self, target, func):
 		self.__target = target
-		Callback.__Evt.Bind(target, wx.ID_ANY, wx.ID_ANY, func)
+		Callback.__Evt.Bind(target, ID_ANY, ID_ANY, func)
 
 	def unbind(self):
-		Callback.__Evt.Unbind(self.__target, wx.ID_ANY, wx.ID_ANY)
+		Callback.__Evt.Unbind(self.__target, ID_ANY, ID_ANY)
 		self.__target = None
 	
 				
-	def start (self, maxval): 
-		self.__last_seen_val = -(self.__interval+1) #-1001
+	def start(self, maxval):
+		self.__last_seen_val = -(self.__interval + 1)
 		self.__trigger(which=Callback.START, maxval=maxval)
 		
 	def update(self, val, data=None):
-		if self.__last_seen_val + self.__interval < val: # Reduce load a bit
+		if self.__last_seen_val + self.__interval < val:  # Reduce load a bit
 			self.__last_seen_val = val
 			self.__trigger(which=Callback.UPDATE, val=val, data=data)
 			
-	def end   (self, state):  
+	def end(self, state):
 		self.__trigger(which=Callback.END, state=state)
 
 
@@ -75,7 +56,7 @@ class Callback:
 	
 
 	'''
-	Private
+	Private implementation
 	'''
 	
 	__EvtCls = None
@@ -83,8 +64,8 @@ class Callback:
 
 	def __trigger(self, **kw):
 		if self.__target:
-			evt = Callback.__EvtCls(**kw)    # Create the event
-			wx.PostEvent(self.__target, evt) # Post the event
+			evt = Callback.__EvtCls(**kw)  # Create the event
+			PostEvent(self.__target, evt)  # Post the event
 
 
 
