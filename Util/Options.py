a4bea90e07818d532804c7053eca73dcdd545e02
SillyBits <sillybits@onlinehome.de>
2019-06-02 12:29:47 +0200

    Complete redo to be type-safe and to allow for hierarchical access

--- Util/Options.py
+++ Util/Options.py
@@ -1,43 +1,36 @@
 """
-Option handling
+Option handling, wrapping :class:`wx.FileConfig` to allow for hierarchical access like
+``cfg.deep_analysis.enabled = False``
+
+@author: SillyBits
+@copyright: (c)2019 SillyBits
+@contact: https://github.com/SillyBits
+@license: GPL
+@version: 1.0
 """
 
 
-import os
+from os \
+	import path
 
-import wx
-
-
-"""
-TODO:
-- Add some upgrading functionality in case format changes dramatically
-  (so its a good idea to store at least some version info)
--
-"""
+from wx \
+	import Config, ConfigBase, FileConfig, CONFIG_USE_LOCAL_FILE
 
 
-class Options(wx.FileConfig):
-	'''
-	Wrapping wx.FileConfig with easy accessors
-	'''
+class Options(FileConfig):
+	"""
+	@param appname: Name of application, used to define name of .ini to process
+	@param apppath: Path where .ini should be stored
+	"""
 
 	def __init__(self, appname:str, apppath:str):
-		inifile = os.path.join(apppath, appname + ".ini")
-		super().__init__(appname, style=wx.CONFIG_USE_LOCAL_FILE, localFilename=inifile)
-		wx.Config.Set(self)
+		inifile = path.join(apppath, appname + ".ini")
+		super().__init__(appname, style=CONFIG_USE_LOCAL_FILE, localFilename=inifile)
+		Config.Set(self)
 
-		self.__mapping = {
-			wx.ConfigBase.EntryType.Type_Float:   [ self.ReadFloat, self.WriteFloat ],
-			wx.ConfigBase.EntryType.Type_Boolean: [ self.ReadBool , self.WriteBool  ],
-			wx.ConfigBase.EntryType.Type_Integer: [ self.ReadInt  , self.WriteInt   ],
-			wx.ConfigBase.EntryType.Type_String:  [ self.Read     , self.Write      ],
-		}
-		self.__names = {}
+		self.__root = Container("/")
 
-		if self.NumberOfEntries < 4:
-			self.__setup()
-		else:
-			self.__load()
+		self.__setup()
 
 
 	'''
@@ -45,59 +38,230 @@ class Options(wx.FileConfig):
 	'''
 
 	def __setup(self):
-		#TODO: Also check if this instance needs an upgrade to a newer version
-		self.__create("deep_analysis", False)
-		self.__create("deep_analysis_asked", False)
-		self.__create("incident_report", False)
-		self.__create("incident_report_asked", False)
+		num = self.GetNumberOfEntries(True)
+		if num < 5:
+			# Create initial config
+			self.version = "v0.3-alpha"
+			self.deep_analysis.enabled = False
+			self.deep_analysis.asked = False
+			self.incident_report.enabled = False
+			self.incident_report.asked = False
+			self.Flush()
+			return
+		# Upgrade existing
+		#TODO:
 
+
+	def __getattr__(self, name:str):
+		if name.startswith("_"):
+			try:
+				return FileConfig.__getattribute__(self, name)
+			except:
+				pass
+		return self.__root.get(name, None)
+
+	def __setattr__(self, name:str, value):
+		if name.startswith("_"):
+			try:
+				return FileConfig.__setattr__(self, name, value)
+			except:
+				pass
+		return self.__root.set(name, value)
+
+
+class Container:
+
+	def __init__(self, path:str):
+		self.__path = path
+		self.__load()
+
+
+	def get(self, name:str, default):
+		if not name in self.__dict__:
+			return self.__create(name, default)
+		obj = object.__getattribute__(self, name)
+		#return object.__getattribute__(self, name)#.get()
+		if isinstance(obj, Property):
+			obj = obj.get()
+		return obj
+
+	def set(self, name:str, value):
+		if not name in self.__dict__:
+			return self.__create(name, value)
+		#return object.__getattribute__(self, name)#.set(value)
+		obj = object.__getattribute__(self, name)
+		if isinstance(obj, Property):
+			obj = obj.set(value)
+		return obj
+
+
+	'''
+	Private implementation
+	'''
+
+	def __str__(self):
+		s = self.__class__.__qualname__ + " @ " + self.__path
+		return s
 
 	def __load(self):
-		more, name, index = self.GetFirstEntry()
+		#vvvvv DEBUG
+		## We must copy ourself, no .copy() avail, sadfully
+		#prev_dict = [ key for key in self.__dict__.keys() ]
+		#^^^^^ DEBUG
+
+		last_path = Config.Get().GetPath()
+		Config.Get().SetPath(self.__path)
+
+		more, name, index = Config.Get().GetFirstEntry()
 		while more:
-			self.__names[name] = self.GetEntryType(name)
-			more, name, index = self.GetNextEntry(index)
-
-	def __create(self, name, value):
-		if isinstance(value, float):
-			t = wx.ConfigBase.EntryType.Type_Float
-		elif isinstance(value, bool):
-			t = wx.ConfigBase.EntryType.Type_Boolean
-		elif isinstance(value, int):
-			t = wx.ConfigBase.EntryType.Type_Integer
-		else:
-			t = wx.ConfigBase.EntryType.Type_String
-
-		self.__names[name] = t
-
-		op = self.__mapping[t][Options.WRITE]
-		op(name, value)
-
-
-	def __getattr__(self, name):
-		if name.startswith("_"):
-			return super().__getattr__(name)
-		if not name in self.__names:
-			self.__create(name, "")
-		t = self.__names[name]
-		op = self.__mapping[t][Options.READ]
-		return op(name)
-
-	def __setattr__(self, name, value):
-		if name.startswith("_"):
-			super().__setattr__(name, value)
-		else:
-			if not name in self.__names:
-				# Create new entries on-the-fly
-				self.__create(name, value)
+			if SEPARATOR in name:
+				path = self.__path + name
+				name,t = name.split(SEPARATOR)
+				t = t[0].lower()
 			else:
-				t = self.__names[name]
-				op = self.__mapping[t][Options.WRITE]
-				op(name, value)
+				# Note: We should run into this only with very old configs!
+				# Convert wx.Config type mapping to our types
+				# (ends up in string always as FileConfig has no real types)
+				t = Config.Get().GetEntryType(name)
+				if t in WX_MAPPING:
+					t = WX_MAPPING[t]
+				else:
+					t = STRING
+				path = self.__path + name + SEPARATOR + t
+
+			prop = TYPE_MAPPING[t](path)
+			self.__dict__[name] = prop
+
+			more, name, index = Config.Get().GetNextEntry(index)
+
+		# Not sure what happens if we do change path while enumerating,
+		# so we will do this in 2 phases: Gather first, create last
+		groups = []
+		more, name, index = Config.Get().GetFirstGroup()
+		while more:
+			groups.append(name)
+			more, name, index = Config.Get().GetNextGroup(index)
+
+		for name in groups:
+			container = Container(self.__path + name + "/")
+			self.__dict__[name] = container
+
+		Config.Get().SetPath(last_path)
+
+		#vvvvv DEBUG
+		#print("members for path {}:".format(self.__path))
+		#for n in self.__dict__:
+		#	if n not in prev_dict:
+		#		print("-",n,":",self.__dict__[n])
+		#^^^^^ DEBUG
 
 
-	READ = 0
-	WRITE = 1
+	def __create(self, name:str, value):
+		if "/" in name or value is None:
+			new_group = name.split("/", maxsplit=1)[0]
+			container = Container(self.__path + new_group + "/")
+			self.__dict__[new_group] = container
+			if name == new_group:
+				# We're done
+				return container
+			# More keys waiting
+			remain = name[len(new_group)+1:]
+			return container.__create(remain, value)
 
+		if isinstance(value, float):
+			t = FLOAT
+		elif isinstance(value, bool):
+			t = BOOL
+		elif isinstance(value, int):
+			t = INT
+		else:
+			t = STRING
+
+		path = self.__path + name + SEPARATOR + t
+		prop = TYPE_MAPPING[t](path, value)
+		self.__dict__[name] = prop
+		return prop.set(value)
+
+
+	def __getattr__(self, name:str):
+		if name.startswith("_"):
+			try:
+				return object.__getattribute__(self, name)
+			except:
+				pass
+		return self.get(name, None)
+
+	def __setattr__(self, name:str, value):
+		if name.startswith("_"):
+			try:
+				return object.__setattr__(self, name, value)
+			except:
+				pass
+		return self.set(name, value)
+
+
+class Property:
+	def __init__(self, path:str, default=None):
+		self.path = path
+		self.default = default
+
+	def get(self):			pass
+	def set(self, value):	pass
+
+	'''
+	Private implementation
+	'''
+
+	def __str__(self):
+		s = self.__class__.__qualname__ + " @ " + self.path
+		if self.default is not None:
+			s += " (default='{}')".format(self.default)
+		return s
+
+	def __lt__(self, other): return self.get() <  other
+	def __le__(self, other): return self.get() <= other
+	def __eq__(self, other): return self.get() == other
+	def __ne__(self, other): return self.get() != other
+	def __gt__(self, other): return self.get() >  other
+	def __ge__(self, other): return self.get() >= other
+	def __bool__(self):      return self.get() == True
+
+class FloatProperty(Property):
+	def get(self):			return Config.Get().ReadFloat (self.path, self.default or 0.0)
+	def set(self, value):	return Config.Get().WriteFloat(self.path, value)
+
+class BoolProperty(Property):
+	def get(self):			return Config.Get().ReadBool (self.path, self.default or False)
+	def set(self, value):	return Config.Get().WriteBool(self.path, value)
+
+class IntProperty(Property):
+	def get(self):			return Config.Get().ReadInt (self.path, self.default or 0)
+	def set(self, value):	return Config.Get().WriteInt(self.path, value)
+
+class StringProperty(Property):
+	def get(self):			return Config.Get().Read (self.path, self.default or "")
+	def set(self, value):	return Config.Get().Write(self.path, value)
+
+
+SEPARATOR = "." # Using ":" has negative side effect of being escaped -> version\:s=...
+
+FLOAT   = "f"
+BOOL    = "b"
+INT     = "i"
+STRING  = "s"
+
+WX_MAPPING = {
+	ConfigBase.EntryType.Type_Float  : FLOAT,
+	ConfigBase.EntryType.Type_Boolean: BOOL,
+	ConfigBase.EntryType.Type_Integer: INT,
+	ConfigBase.EntryType.Type_String : STRING,
+}
+
+TYPE_MAPPING = {
+	FLOAT : FloatProperty,
+	BOOL  : BoolProperty,
+	INT   : IntProperty,
+	STRING: StringProperty,
+}
 
 
