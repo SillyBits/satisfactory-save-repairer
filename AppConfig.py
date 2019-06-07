b0337904b684aa16d747a5295ba9fbcb0d6ca15f
SillyBits <sillybits@onlinehome.de>
2019-06-07 20:49:00 +0200

    AppConfig: A new home for all those hard-coded values :D

---
+++ AppConfig.py
@@ -0,0 +1,10 @@
+"""
+Static configs for application
+"""
+
+#APP_NAME = "SatisfactorySaveRepairer"
+#APP_TITLE = "Satisfactory Savegame Repairer"
+APP_NAME = "SatisfactorySaveChecker"
+APP_TITLE = "Satisfactory Savegame Checker"
+
+CURR_VERSION = "v0.3-alpha"
